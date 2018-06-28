9d4f4aa5c31b8e8088df0868b127cb5db72e72ef
Stephanie Lampotang <slampotang@fb.com>
2018-06-28 10:59:33 -0700

    error,  but the search page is technically created

---
+++ Flix/ViewControllers/SearchViewController.h
@@ -0,0 +1,12 @@
+//
+//  SearchViewController.h
+//  
+//
+//  Created by Stephanie Lampotang on 6/28/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UITableViewController
+
+@end
