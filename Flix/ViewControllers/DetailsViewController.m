c30370463d33406dcd3fa0bcc9718330cb4443f5
Stephanie Lampotang <slampotang@fb.com>
2018-06-29 18:23:45 -0700

    trailer in the details ahhhHHHH

--- Flix/ViewControllers/DetailsViewController.m
+++ Flix/ViewControllers/DetailsViewController.m
@@ -12,6 +12,7 @@
 #import "TrailerViewController.h"
 #import <QuartzCore/QuartzCore.h>
 #import "FavoriteViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface DetailsViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *backdropView;
@@ -21,6 +22,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundView;
+@property (weak, nonatomic) IBOutlet WKWebView *webView;
+@property (nonatomic, strong) NSArray *trailers;
 
 @end
 
@@ -55,9 +58,57 @@
     self.synopsisLabel.numberOfLines = 0;
     [self.synopsisLabel sizeToFit];
     
-    CGFloat maxHeight = self.synopsisLabel.frame.origin.y + self.synopsisLabel.frame.size.height;
+    CGRect newFrame = self.webView.frame;
+    newFrame.origin.y = self.synopsisLabel.frame.origin.y + self.synopsisLabel.frame.size.height + 15;
+    self.webView.frame = newFrame;
+    
+//    newFrame.origin.y = CGOriginMake(self.synopsisLabel.frame.origin.y + self.synopsisLabel.frame.size.height);
+    
+    CGFloat maxHeight = self.synopsisLabel.frame.origin.y + self.synopsisLabel.frame.size.height + self.webView.frame.size.height;
     
     self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width, maxHeight);
+    
+    NSString *one = @"https://api.themoviedb.org/3/movie/";
+    NSString *two = [NSString stringWithFormat:@"%@", self.movie[@"id"]];
+    NSString *three = [one stringByAppendingString:two];
+    
+    NSString *final = [three stringByAppendingString:@"/videos?api_key=60e1d3f9485ce67b2f7c2f12b424a95d&language=en-US"];
+    NSURL *url = [NSURL URLWithString:final];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            NSLog(@"%@", dataDictionary);
+            // TODO: Get the array of movies // wants to store results in an array of movies
+            self.trailers = dataDictionary[@"results"];
+            //            for (NSDictionary *id in self.trailers) {
+            //                NSLog(@"%@", id);
+            //                NSLog(@"here");
+            //            }
+            
+            // Do any additional setup after loading the view.
+            NSString *urlString = @"https://www.youtube.com/watch?v=";
+            NSString *urlString2 = [urlString stringByAppendingString:self.trailers[0][@"key"]];
+            // Convert the url String to a NSURL object.
+            NSURL *url = [NSURL URLWithString:urlString2];
+            
+            // Place the URL in a URL Request.
+            NSURLRequest *request = [NSURLRequest requestWithURL:url
+                                                     cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
+                                                 timeoutInterval:10.0];
+            // Load Request into WebView.
+            
+            [self.webView loadRequest:request];
+//            [self.activityIndicator stopAnimating];
+        }
+    }];
+    
+    [task resume];
 }
 
 - (void)didReceiveMemoryWarning {
--- Flix/ViewControllers/TrailerViewController.m
+++ Flix/ViewControllers/TrailerViewController.m
@@ -10,6 +10,7 @@
 #import <WebKit/WebKit.h>
 
 @interface TrailerViewController ()
+//@property (weak, nonatomic) IBOutlet WKWebView *webView;
 @property (weak, nonatomic) IBOutlet WKWebView *webView;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (nonatomic, strong) NSArray *trailers;
@@ -29,7 +30,7 @@
     NSString *one = @"https://api.themoviedb.org/3/movie/";
     NSString *two = [NSString stringWithFormat:@"%@", self.movie[@"id"]];
     NSString *three = [one stringByAppendingString:two];
-    
+
     NSString *final = [three stringByAppendingString:@"/videos?api_key=60e1d3f9485ce67b2f7c2f12b424a95d&language=en-US"];
     NSURL *url = [NSURL URLWithString:final];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -40,7 +41,7 @@
         }
         else {
             NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-            
+
             NSLog(@"%@", dataDictionary);
             // TODO: Get the array of movies // wants to store results in an array of movies
             self.trailers = dataDictionary[@"results"];
@@ -48,24 +49,24 @@
 //                NSLog(@"%@", id);
 //                NSLog(@"here");
 //            }
-            
+
             // Do any additional setup after loading the view.
             NSString *urlString = @"https://www.youtube.com/watch?v=";
             NSString *urlString2 = [urlString stringByAppendingString:self.trailers[0][@"key"]];
             // Convert the url String to a NSURL object.
             NSURL *url = [NSURL URLWithString:urlString2];
-            
+
             // Place the URL in a URL Request.
             NSURLRequest *request = [NSURLRequest requestWithURL:url
                                                      cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
                                                  timeoutInterval:10.0];
             // Load Request into WebView.
-            
+
             [self.webView loadRequest:request];
             [self.activityIndicator stopAnimating];
         }
     }];
-    
+
     [task resume];
 }
 
